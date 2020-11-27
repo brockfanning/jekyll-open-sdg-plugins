48d99d008fdee32e1a22c2956f232fa805194166
Brock Fanning <brockfanning@gmail.com>
2020-11-27 14:48:58 -0500

    Partial progress - probably not pursuing this

--- lib/jekyll-open-sdg-plugins/fetch_remote_data.rb
+++ lib/jekyll-open-sdg-plugins/fetch_remote_data.rb
@@ -87,80 +87,114 @@ module JekyllOpenSdgPlugins
         site.data['translations'] = {}
       end
 
-      remote = site.config['remote_data_prefix']
-      local = site.config['local_data_folder']
-
-      if !remote && !local
-        abort 'Site config must include "remote_data_prefix".'
+      frameworks = { 'sdg' => '' }
+      if site.config.has_key?('alternate_frameworks')
+        site.config['alternate_frameworks'].each do |framework|
+          frameworks[framework['id']] = framework['data_prefix']
+        end
       end
 
-      build_location = remote ? remote : local
-      is_remote = opensdg_is_path_remote(build_location)
+      frameworks.each do |framework, framework_prefix|
 
-      build_location = is_remote ? build_location : File.join(Dir.pwd, build_location)
-      translated_builds = site_uses_translated_builds(build_location)
+        is_alt_framework = framework != 'sdg'
 
-      if translated_builds
-        # For translated builds, we get a build for each language, and
-        # place them in "subfolders" (so to speak) of site.data.
-        subfolders = site.config['languages'].clone
-        subfolders.append('untranslated')
-        subfolders.each do |language|
-          data_target = site.data[language]
-          translated_build = is_remote ? build_location + '/' + language : File.join(build_location, language)
-          data_source = fetch_build(translated_build)
-          if !data_source.empty?
-            if data_target
-              data_target.deep_merge(data_source)
-            else
-              site.data[language] = data_source
+        if is_alt_framework
+          build_location = framework_prefix
+          if !site.data.has_key?(framework)
+            site.data[framework] = {}
+          end
+        else
+          remote = site.config['remote_data_prefix']
+          local = site.config['local_data_folder']
+
+          if !remote && !local
+            abort 'Site config must include "remote_data_prefix".'
+          end
+
+          build_location = remote ? remote : local
+        end
+
+        is_remote = opensdg_is_path_remote(build_location)
+        build_location = is_remote ? build_location : File.join(Dir.pwd, build_location)
+        translated_builds = site_uses_translated_builds(build_location)
+
+        if translated_builds
+          # For translated builds, we get a build for each language, and
+          # place them in "subfolders" (so to speak) of site.data.
+          subfolders = site.config['languages'].clone
+          subfolders.append('untranslated')
+
+          subfolders.each do |language|
+            data_target = is_alt_framework ?  site.data[framework][language] : site.data[language]
+            translated_build = is_remote ? build_location + '/' + language : File.join(build_location, language)
+            data_source = fetch_build(translated_build)
+            if !data_source.empty?
+              if data_target
+                data_target.deep_merge(data_source)
+              else
+                if is_alt_framework
+                  site.data[framework][language] = data_source
+                else
+                  site.data[language] = data_source
+                end
+              end
             end
           end
-        end
-        # We move the language-specific translations to the
-        # site.data.translations location, where all translations are kept.
-        site.config['languages'].each do |language|
-          translation_target = site.data['translations'][language]
-          translation_source = site.data[language]['translations']
-          if translation_target
-            translation_target.deep_merge(translation_source)
+          # We move the language-specific translations to the
+          # site.data.translations location, where all translations are kept.
+          site.config['languages'].each do |language|
+            translation_target = is_alt_framework ? site.data[framework]['translations'][language] : site.data['translations'][language]
+            translation_source = is_alt_framework ? site.data[framework]['translations'] : site.data[language]['translations']
+            if translation_target
+              translation_target.deep_merge(translation_source)
+            else
+              if is_alt_framework
+                site.data[framework]['translations'][language] = translation_source
+              else
+                site.data['translations'][language] = translation_source
+              end
+            end
+          end
+          # And there are some parts of the build that don't need to be translated
+          # and should be moved to the top level.
+          first_language = site.config['languages'][0]
+          if is_alt_framework
+            site.data[framework]['reporting'] = site.data[framework][first_language]['reporting']
+            site.data[framework]['schema'] = site.data[framework][first_language]['schema']
+            site.data[framework]['zip'] = site.data[framework][first_language]['zip']
           else
-            site.data['translations'][language] = translation_source
+            site.data['reporting'] = site.data[first_language]['reporting']
+            site.data['schema'] = site.data[first_language]['schema']
+            site.data['zip'] = site.data[first_language]['zip']
+          end
+        else
+          # For untranslated builds, we download one build only, and place it
+          # in the "root" (so to speak) of site.data. Nothing else is needed.
+          target = is_alt_framework ? site.data[framework] : site.data
+          source = fetch_build(build_location)
+          if !source.empty?
+            target.deep_merge(source)
           end
         end
-        # And there are some parts of the build that don't need to be translated
-        # and should be moved to the top level.
-        first_language = site.config['languages'][0]
-        site.data['reporting'] = site.data[first_language]['reporting']
-        site.data['schema'] = site.data[first_language]['schema']
-        site.data['zip'] = site.data[first_language]['zip']
-      else
-        # For untranslated builds, we download one build only, and place it
-        # in the "root" (so to speak) of site.data. Nothing else is needed.
-        target = site.data
-        source = fetch_build(build_location)
-        if !source.empty?
-          target.deep_merge(source)
-        end
-      end
 
-      # Finally support the deprecated 'remote_translations' option.
-      # This is deprecated because translations should now be in the
-      # data repository, where they will be fetched in fetch_build().
-      if site.config['remote_translations']
-        key = 'translations'
-        target = site.data[key]
-        site.config['remote_translations'].each do |endpoint|
-          begin
-            source = JSON.load(open(endpoint))
-            if target
-              target.deep_merge(source)
-            else
-              site.data[key] = source
+        # Finally support the deprecated 'remote_translations' option.
+        # This is deprecated because translations should now be in the
+        # data repository, where they will be fetched in fetch_build().
+        if site.config['remote_translations']
+          key = 'translations'
+          target = site.data[key]
+          site.config['remote_translations'].each do |endpoint|
+            begin
+              source = JSON.load(open(endpoint))
+              if target
+                target.deep_merge(source)
+              else
+                site.data[key] = source
+              end
+            rescue StandardError => e
+              puts e.message
+              abort 'Unable to fetch remote translation from: ' + endpoint
             end
-          rescue StandardError => e
-            puts e.message
-            abort 'Unable to fetch remote translation from: ' + endpoint
           end
         end
       end
