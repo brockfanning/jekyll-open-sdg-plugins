5cb5f61a1bc8446dce1a463079e14dd109026585
Brock Fanning <1319083+brockfanning@users.noreply.github.com>
2025-04-19 22:56:41 -0400

    Debugging

--- lib/jekyll-open-sdg-plugins/translate_site_configuration.rb
+++ lib/jekyll-open-sdg-plugins/translate_site_configuration.rb
@@ -9,6 +9,15 @@ module JekyllOpenSdgPlugins
     # Some site configuration settings need to be translated.
     def generate(site)
 
+        if site.data['data_fields'].nil?
+            site.data['data_fields'] = {}
+        end
+        if site.data['data_fields']['reportingtype'].nil?
+            site.data['data_fields']['reportingtype'] = 'REPORTING_TYPE'
+            site.data['data_fields']['reportingtype_national'] = 'N'
+            site.data['data_fields']['reportingtypeglobal'] = 'G'
+        end
+
         site.data['translated_site_config'] = {}
         site.config['languages'].each_with_index do |language, index|
             puts language
